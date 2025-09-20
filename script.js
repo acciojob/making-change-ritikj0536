e0a65fdbb2cf3847e7c31747a11502a3a26e99ac
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-09-20 20:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const makeChange = (c) => {
-  // your name here
+  c = Number(c); // ensure input is a number
+
+  let change = {
+    q: 0, // quarters
+    d: 0, // dimes
+    n: 0, // nickels
+    p: 0  // pennies
+  };
+
+  change.q = Math.floor(c / 25);
+  c %= 25;
+
+  change.d = Math.floor(c / 10);
+  c %= 10;
+
+  change.n = Math.floor(c / 5);
+  c %= 5;
+
+  change.p = c;
+
+  return change;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
